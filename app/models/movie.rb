c6de861eab1942d66c84c842e61b1fc8090fc2ed
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-03-01 19:23:56 +0000

    complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,45 @@
 class Movie < ActiveRecord::Base
     def self.create_with_title(title)
-        self.title = title
-        Movie.create(title: title)
+        self.create(title: title)
+    end
+
+    def self.first_movie
+        self.all.first
+    end
+
+    def self.last_movie
+        self.all.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(attr)
+        self.find_by(attr)
+    end
+
+    def self.find_movies_after_2002
+        self.where "release_date > ?", 2002
+    end
+
+    def update_with_attributes(attr)
+        self.attributes = attr
+    end
+
+    def self.update_all_titles(attr)
+        self.update_all title: attr
+    end
+
+    def self.delete_by_id(id)
+        self.destroy(id)
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
     end
 end
